6413f54e325672a96301e78b1fdb9cd7f00bdf30
abdelhameed <abdelhameedbasyouni@gmail.com>
2022-12-26 22:06:24 +0200

    adding navbar,sidebar and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LastFewTransactionsComponent } from './components/last-few-transactions/last-few-transactions.component';
+import { MainComponent } from './components/main/main.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { TopWidgetsComponent } from './components/top-widgets/top-widgets.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    MainComponent,
+    TopWidgetsComponent,
+    LastFewTransactionsComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
