0d0d5587fac1155d740548f55b62db14ddbf0833
abdelhameed <abdelhameedbasyouni@gmail.com>
2023-01-01 04:32:33 +0200

    adding charts and editing style

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  myChart: any;
+  myChart2:any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.myChart = new Chart("myChart",{type: 'doughnut',data : {
+      labels: [
+        'Red',
+        'Blue',
+        'Yellow'
+      ],
+      datasets: [{
+        label: 'My First Dataset',
+        data: [300, 100, 200],
+        backgroundColor: [
+          'rgb(255, 99, 132)',
+          'rgb(54, 162, 235)',
+          'rgb(255, 205, 86)'
+        ],
+        hoverOffset: 4
+      }]
+    }})
+    this.myChart2 = new Chart("myChart2",{type: 'line',data : {
+      labels: ["HP","Dell","Huwawi","Toshipa","HP"],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56],
+    fill: false,
+    borderColor: 'rgb(75, 192, 192)',
+    tension: 0.1
+  }]
+    }})
   }
 
 }
